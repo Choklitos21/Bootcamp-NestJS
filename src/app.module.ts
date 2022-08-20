95909add97f21aecdbb1a46ce702e3f2d90d8c38
DiegoMorales <diego.mm09@hotmail.com>
2022-08-20 13:01:54 -0500

    Bootcamp NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {AppConfigModule} from "./config/app/config.module";
+import {TypeOrmConfigModule} from "./config/typeorm/typeorm.module";
+import {DatabaseConfigModule} from "./config/database/config.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./database/entities/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmConfigModule,
+      TypeOrmModule.forFeature([User]),
+      DatabaseConfigModule,
+      AppConfigModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {Get, Injectable} from '@nestjs/common';
+import { IResponse } from "./app.controller";
+import {AppConfigService} from "./config/app/config.service";
+import {User} from "./database/entities/user.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(
+      private readonly appConfig: AppConfigService,
+      @InjectRepository(User)
+      private readonly userRepo: Repository<User>
+  ) {
+  }
+
+  getHello(): IResponse {
+    return {
+      appName: this.appConfig.name,
+      appUrl: this.appConfig.url,
+      appPort: this.appConfig.port,
+    };
+  }
+
+  sayHi(): string{
+    return 'Buenas tardes'
+  }
+
+  async getAllUsers(): Promise<User[]>{
+    return await this.userRepo.find()
+  }
+
+  async getUser(userId): Promise<User>{
+    return await this.userRepo.findOne(userId)
   }
 }
